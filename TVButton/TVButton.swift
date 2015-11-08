3bbca61bfeb32a8f3c66e319fac94cb2bed723ea
Roy Marmelstein <marmelroy@gmail.com>
2015-11-08 08:17:34 +0100

    Exits are final

--- TVButton/TVButton.swift
+++ TVButton/TVButton.swift
@@ -30,20 +30,22 @@ public extension TVButtonLayer {
  - shadowColor: Provide the dominant colour of your background for an even better shadow.
 
  */
-public class TVButton: UIButton {
+public class TVButton: UIButton, UIGestureRecognizerDelegate {
     
     var specularView = UIImageView()
     var containerView = UIView()
     var panGestureRecognizer: UIPanGestureRecognizer?
     var tapGestureRecognizer: UITapGestureRecognizer?
     var longPressGestureRecognizer: UILongPressGestureRecognizer?
+    var highlightMode: Bool = false
 
     let maxRotation: CGFloat = 10
     let maxTranslation: CGFloat = 2
-    let specularScale: CGFloat = 1.5
+    let specularScale: CGFloat = 2.0
     let highlightedScale: CGFloat = 1.17
     let shadowFactor: CGFloat = 12
-
+    let cornerRadius: CGFloat = 5
+    
     public var shadowColor : UIColor? {
         didSet {
             self.layer.shadowColor = shadowColor!.CGColor
@@ -58,7 +60,7 @@ public class TVButton: UIButton {
             }
             for layer in layers! {
                 let imageView = UIImageView(image: layer.internalImage)
-                imageView.layer.cornerRadius = 5
+                imageView.layer.cornerRadius = cornerRadius
                 imageView.clipsToBounds = true
                 containerView.addSubview(imageView)
             }
@@ -94,7 +96,7 @@ public class TVButton: UIButton {
             }
         }
         self.layer.masksToBounds = false;
-        let shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: 5)
+        let shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: cornerRadius)
         self.layer.shadowPath = shadowPath.CGPath
     }
     
@@ -102,18 +104,22 @@ public class TVButton: UIButton {
     func setup() {
         containerView.userInteractionEnabled = false
         self.addSubview(containerView)
+        containerView.clipsToBounds = true
+        containerView.layer.cornerRadius = cornerRadius
         self.clipsToBounds = true
         specularView.alpha = 0.0
         specularView.contentMode = UIViewContentMode.ScaleAspectFill
         self.shadowColor = UIColor.blackColor()
-        self.layer.shadowRadius  = self.bounds.size.height/(2*shadowFactor)
-        self.layer.shadowOffset  = CGSizeMake(0.0, 0.0)
+        self.layer.shadowRadius = self.bounds.size.height/(2*shadowFactor)
+        self.layer.shadowOffset = CGSizeMake(0.0, shadowFactor/3)
         self.layer.shadowOpacity = 0.5;
         panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        panGestureRecognizer?.delegate = self
         self.addGestureRecognizer(panGestureRecognizer!)
         tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
         self.addGestureRecognizer(tapGestureRecognizer!)
         longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        longPressGestureRecognizer?.delegate = self
         self.addGestureRecognizer(longPressGestureRecognizer!)
     }
     
@@ -124,9 +130,8 @@ public class TVButton: UIButton {
         if layers == nil {
             return
         }
-        self.highlighted = false
         if gestureRecognizer.state == .Began {
-            self.enterMovement()
+            self.enterMovement(0.4)
         }
         else if gestureRecognizer.state == .Changed {
             self.processMovement(gestureRecognizer)
@@ -141,9 +146,11 @@ public class TVButton: UIButton {
         if layers == nil {
             return
         }
-        self.highlighted = false
         if gestureRecognizer.state == .Began {
-            self.enterMovement()
+            self.enterMovement(0.2)
+        }
+        else {
+            self.exitMovement()
         }
     }
     
@@ -155,7 +162,13 @@ public class TVButton: UIButton {
     
     // MARK: Animations
     
-    func enterMovement() {
+    func enterMovement(duration: Double) {
+        if highlightMode == true {
+            return
+        }
+        else {
+            highlightMode = true
+        }
         let targetScaleTransform = CATransform3DMakeScale(highlightedScale, highlightedScale, highlightedScale)
         let targetShadowOffset = CGSizeMake(0.0, self.bounds.size.height/shadowFactor)
         CATransaction.begin()
@@ -166,14 +179,14 @@ public class TVButton: UIButton {
         })
         let scaleAnimation = CABasicAnimation(keyPath: "transform")
         scaleAnimation.toValue = NSValue(CATransform3D: targetScaleTransform)
-        scaleAnimation.duration = 0.4
+        scaleAnimation.duration = duration
         scaleAnimation.removedOnCompletion = false
         scaleAnimation.fillMode = kCAFillModeForwards
         scaleAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
         self.layer.addAnimation(scaleAnimation, forKey: "scaleAnimation")
         let shaowOffsetAnimation = CABasicAnimation(keyPath: "shadowOffset")
         shaowOffsetAnimation.toValue = NSValue(CGSize: targetShadowOffset)
-        shaowOffsetAnimation.duration = 0.4
+        shaowOffsetAnimation.duration = duration
         shaowOffsetAnimation.removedOnCompletion = false
         shaowOffsetAnimation.fillMode = kCAFillModeForwards
         shaowOffsetAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
@@ -182,6 +195,7 @@ public class TVButton: UIButton {
     }
     
     func animateTap() {
+        highlightMode = false
         UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
             self.transform = CGAffineTransformMakeScale(0.93, 0.93)
             }) { (finished) -> Void in
@@ -194,13 +208,20 @@ public class TVButton: UIButton {
     }
     
     func exitMovement() {
+        if highlightMode == false || longPressGestureRecognizer?.state == .Began || longPressGestureRecognizer?.state == .Changed || panGestureRecognizer?.state == .Began || panGestureRecognizer?.state == .Changed  {
+            return
+        }
+        else {
+            highlightMode = false
+        }
+        let targetShadowOffset = CGSizeMake(0.0, shadowFactor/3)
         CATransaction.begin()
         CATransaction.setCompletionBlock({ () -> Void in
-            self.layer.shadowOffset = CGSizeZero
+            self.layer.shadowOffset = targetShadowOffset
             self.layer.removeAllAnimations()
         })
         let shaowOffsetAnimation = CABasicAnimation(keyPath: "shadowOffset")
-        shaowOffsetAnimation.toValue = NSValue(CGSize: CGSizeZero)
+        shaowOffsetAnimation.toValue = NSValue(CGSize: targetShadowOffset)
         shaowOffsetAnimation.duration = 0.4
         shaowOffsetAnimation.fillMode = kCAFillModeForwards
         shaowOffsetAnimation.removedOnCompletion = false
@@ -216,11 +237,14 @@ public class TVButton: UIButton {
             }
         }, completion:nil)
     }
-
+    
+    public func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
     
     func processMovement(gestureRecognizer: UIGestureRecognizer){
         let point = gestureRecognizer.locationInView(self)
-        if (CGRectContainsPoint(self.bounds, point) == false) {
+        if (CGRectContainsPoint(self.bounds, point) == false || highlightMode == false) {
             return
         }
         let xRotationDegrees = (calculateTransform(point.x, dimension: self.bounds.size.width)) * maxRotation
@@ -238,16 +262,19 @@ public class TVButton: UIButton {
         combinedRotateTransform.m34 = 1.0 / 1000.0
         
         let translationTransform = CATransform3DMakeTranslation(-xTranslation, yTranslation, 0.0)
-        let combinedTransform = CATransform3DConcat(combinedRotateTransform, translationTransform)
+        let combinedRotateTranslateTransform = CATransform3DConcat(combinedRotateTransform, translationTransform)
+        let targetScaleTransform = CATransform3DMakeScale(highlightedScale, highlightedScale, highlightedScale)
+        let combinedTransform = CATransform3DConcat(combinedRotateTranslateTransform, targetScaleTransform)
+
         UIView.animateWithDuration(0.4, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
             self.layer.transform = combinedTransform
             self.specularView.alpha = 0.3
             self.specularView.center = point
             }, completion: nil)
-        for var i = 0; i < containerView.subviews.count ; i++ {
+        for var i = 1; i < containerView.subviews.count ; i++ {
             let subview = containerView.subviews[i]
             if subview != specularView {
-                subview.center = CGPointMake(self.bounds.size.width/2 + xTranslation*CGFloat(i), self.bounds.size.height/2 + yTranslation*CGFloat(i))
+                subview.center = CGPointMake(self.bounds.size.width/2 + xTranslation*CGFloat(i)*0.5, self.bounds.size.height/2 + yTranslation*CGFloat(i)*0.5)
             }
         }
     }
