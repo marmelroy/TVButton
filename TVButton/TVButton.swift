dc50e1f3899b1ceb331b01feb58c1fa8e8a496d4
Roy Marmelstein <marmelroy@gmail.com>
2015-11-08 09:35:07 +0100

    Optimising

--- TVButton/TVButton.swift
+++ TVButton/TVButton.swift
@@ -38,8 +38,11 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     var tapGestureRecognizer: UITapGestureRecognizer?
     var longPressGestureRecognizer: UILongPressGestureRecognizer?
     var highlightMode: Bool = false
-
-    let maxRotation: CGFloat = 10
+    
+    let animationDuration: Double = 0.4
+    let rotateYFactor: CGFloat = 16
+    let rotateXFactor: CGFloat = 20
+    let rotateZFactor: CGFloat = 9
     let maxTranslation: CGFloat = 2
     let specularScale: CGFloat = 2.0
     let highlightedScale: CGFloat = 1.17
@@ -131,7 +134,7 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
             return
         }
         if gestureRecognizer.state == .Began {
-            self.enterMovement(0.4)
+            self.enterMovement()
         }
         else if gestureRecognizer.state == .Changed {
             self.processMovement(gestureRecognizer)
@@ -147,7 +150,8 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
             return
         }
         if gestureRecognizer.state == .Began {
-            self.enterMovement(0.2)
+            self.enterMovement()
+            self.processMovement(gestureRecognizer)
         }
         else {
             self.exitMovement()
@@ -156,37 +160,25 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     
     func handleTap(gestureRecognizer: UIGestureRecognizer) {
         super.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
-        self.animateTap()
     }
     
     
     // MARK: Animations
     
-    func enterMovement(duration: Double) {
+    func enterMovement() {
         if highlightMode == true {
             return
         }
-        else {
-            highlightMode = true
-        }
-        let targetScaleTransform = CATransform3DMakeScale(highlightedScale, highlightedScale, highlightedScale)
+        self.highlightMode = true
         let targetShadowOffset = CGSizeMake(0.0, self.bounds.size.height/shadowFactor)
+        self.layer.removeAllAnimations()
         CATransaction.begin()
         CATransaction.setCompletionBlock({ () -> Void in
-            self.layer.transform = targetScaleTransform
             self.layer.shadowOffset = targetShadowOffset
-            self.layer.removeAllAnimations()
         })
-        let scaleAnimation = CABasicAnimation(keyPath: "transform")
-        scaleAnimation.toValue = NSValue(CATransform3D: targetScaleTransform)
-        scaleAnimation.duration = duration
-        scaleAnimation.removedOnCompletion = false
-        scaleAnimation.fillMode = kCAFillModeForwards
-        scaleAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
-        self.layer.addAnimation(scaleAnimation, forKey: "scaleAnimation")
         let shaowOffsetAnimation = CABasicAnimation(keyPath: "shadowOffset")
         shaowOffsetAnimation.toValue = NSValue(CGSize: targetShadowOffset)
-        shaowOffsetAnimation.duration = duration
+        shaowOffsetAnimation.duration = animationDuration
         shaowOffsetAnimation.removedOnCompletion = false
         shaowOffsetAnimation.fillMode = kCAFillModeForwards
         shaowOffsetAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
@@ -194,41 +186,37 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
         CATransaction.commit()
     }
     
-    func animateTap() {
-        highlightMode = false
-        UIView.animateWithDuration(0.3, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
-            self.transform = CGAffineTransformMakeScale(0.93, 0.93)
-            }) { (finished) -> Void in
-                UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
-                    self.transform = CGAffineTransformMakeScale(1.0, 1.0)
-                    }) { (finished) -> Void in
-                        
-                }
-        }
-    }
     
     func exitMovement() {
         if highlightMode == false || longPressGestureRecognizer?.state == .Began || longPressGestureRecognizer?.state == .Changed || panGestureRecognizer?.state == .Began || panGestureRecognizer?.state == .Changed  {
             return
         }
-        else {
-            highlightMode = false
-        }
         let targetShadowOffset = CGSizeMake(0.0, shadowFactor/3)
+        let targetScaleTransform = CATransform3DMakeScale(1.0, 1.0, 1.0)
+        self.layer.removeAllAnimations()
+        self.specularView.layer.removeAllAnimations()
         CATransaction.begin()
         CATransaction.setCompletionBlock({ () -> Void in
+            self.layer.transform = targetScaleTransform
             self.layer.shadowOffset = targetShadowOffset
-            self.layer.removeAllAnimations()
+            self.highlightMode = false
         })
         let shaowOffsetAnimation = CABasicAnimation(keyPath: "shadowOffset")
         shaowOffsetAnimation.toValue = NSValue(CGSize: targetShadowOffset)
-        shaowOffsetAnimation.duration = 0.4
+        shaowOffsetAnimation.duration = animationDuration
         shaowOffsetAnimation.fillMode = kCAFillModeForwards
         shaowOffsetAnimation.removedOnCompletion = false
         shaowOffsetAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
         self.layer.addAnimation(shaowOffsetAnimation, forKey: "shadowOffset")
+        let scaleAnimation = CABasicAnimation(keyPath: "transform")
+        scaleAnimation.toValue = NSValue(CATransform3D: targetScaleTransform)
+        scaleAnimation.duration = animationDuration
+        scaleAnimation.removedOnCompletion = false
+        scaleAnimation.fillMode = kCAFillModeForwards
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: "easeOut")
+        self.layer.addAnimation(scaleAnimation, forKey: "scaleAnimation")
         CATransaction.commit()
-        UIView.animateWithDuration(0.4, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
             self.transform = CGAffineTransformIdentity
             self.specularView.alpha = 0.0
             for var i = 0; i < self.containerView.subviews.count ; i++ {
@@ -244,44 +232,46 @@ public class TVButton: UIButton, UIGestureRecognizerDelegate {
     
     func processMovement(gestureRecognizer: UIGestureRecognizer){
         let point = gestureRecognizer.locationInView(self)
-        if (CGRectContainsPoint(self.bounds, point) == false || highlightMode == false) {
+        if (highlightMode == false) {
             return
         }
-        let xRotationDegrees = (calculateTransform(point.x, dimension: self.bounds.size.width)) * maxRotation
-        let yRotationDegrees = (calculateTransform(point.y, dimension: self.bounds.size.height) * -1) * maxRotation
-        let xTranslation = (calculateTransform(point.x, dimension: self.bounds.size.width)) * maxTranslation
-        let yTranslation = (calculateTransform(point.y, dimension: self.bounds.size.height)) * maxTranslation
+        let offsetX = point.x / self.bounds.size.width
+        let offsetY = point.y / self.bounds.size.height
+        let dx = point.x - self.bounds.size.width/2
+        let dy = point.y - self.bounds.size.height/2
+        let xRotation = (dy - offsetY)*(rotateXFactor/self.bounds.size.width)
+        let yRotation = (offsetX - dx)*(rotateYFactor/self.bounds.size.width)
+        let zRotation = (xRotation + yRotation)/rotateZFactor
+
+        let xTranslation = (-2*point.x/self.bounds.size.width)*maxTranslation
+        let yTranslation = (-2*point.y/self.bounds.size.height)*maxTranslation
         
-        let translationAverage = (calculateTransform(point.y, dimension: self.bounds.size.height) + calculateTransform(point.x, dimension: self.bounds.size.width))/2
-        print(translationAverage)
-        
-        let xRotateTransform = CATransform3DMakeRotation(degreesToRadians(xRotationDegrees), 1, 0, 0)
-        let yRotateTransform = CATransform3DMakeRotation(degreesToRadians(yRotationDegrees), 0, 1, 0)
-        
-        var combinedRotateTransform = CATransform3DConcat(xRotateTransform, yRotateTransform)
-        combinedRotateTransform.m34 = 1.0 / 1000.0
-        
+        let xRotateTransform = CATransform3DMakeRotation(degreesToRadians(xRotation), 1, 0, 0)
+        let yRotateTransform = CATransform3DMakeRotation(degreesToRadians(yRotation), 0, 1, 0)
+        let zRotateTransform = CATransform3DMakeRotation(degreesToRadians(zRotation), 0, 0, 1)
+
+        let combinedRotateTransformXY = CATransform3DConcat(xRotateTransform, yRotateTransform)
+        let combinedRotateTransformZ = CATransform3DConcat(combinedRotateTransformXY, zRotateTransform)
         let translationTransform = CATransform3DMakeTranslation(-xTranslation, yTranslation, 0.0)
-        let combinedRotateTranslateTransform = CATransform3DConcat(combinedRotateTransform, translationTransform)
+        let combinedRotateTranslateTransform = CATransform3DConcat(combinedRotateTransformZ, translationTransform)
         let targetScaleTransform = CATransform3DMakeScale(highlightedScale, highlightedScale, highlightedScale)
         let combinedTransform = CATransform3DConcat(combinedRotateTranslateTransform, targetScaleTransform)
 
-        UIView.animateWithDuration(0.4, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
             self.layer.transform = combinedTransform
             self.specularView.alpha = 0.3
-            self.specularView.center = point
-            }, completion: nil)
-        for var i = 1; i < containerView.subviews.count ; i++ {
-            let subview = containerView.subviews[i]
-            if subview != specularView {
-                subview.center = CGPointMake(self.bounds.size.width/2 + xTranslation*CGFloat(i)*0.5, self.bounds.size.height/2 + yTranslation*CGFloat(i)*0.5)
+            for var i = 1; i < self.containerView.subviews.count ; i++ {
+                let subview = self.containerView.subviews[i]
+                if subview != self.specularView {
+                    subview.center = CGPointMake(self.bounds.size.width/2 + xTranslation*CGFloat(i)*0.5, self.bounds.size.height/2 + yTranslation*CGFloat(i)*0.5)
+                }
             }
-        }
+            self.specularView.center = point
+        }, completion: nil)
+
     }
     
-    func calculateTransform(offset: CGFloat, dimension: CGFloat) -> CGFloat{
-        return (-2/dimension)*offset + 1
-    }
+    // MARK: Convenience
     
     func degreesToRadians(value:CGFloat) -> CGFloat {
         return value * CGFloat(M_PI) / 180.0
