c6909fbbff48be5bbc9aafcb7194b4bfab9f09e9
Roy Marmelstein <marmelroy@gmail.com>
2015-11-10 12:27:13 +0100

    Constant comments

--- TVButton/TVButtonConstants.swift
+++ TVButton/TVButtonConstants.swift
@@ -8,25 +8,38 @@
 
 import Foundation
 
+// Affects reveal, hide and specular shine following animations
 let animationDuration: Double = 0.4
 
+// The corner radius of the button
 let cornerRadius: CGFloat = 5
 
+// Default parallax intensity
 let defaultParallaxIntensity: CGFloat = 1.0
 
+// The scaling on the TVButton when active
 let highlightedScale: CGFloat = 1.17
 
+// Max translation vlaue
 let maxTranslation: CGFloat = 2
 
+// A factor applied in the x axis on parallax intensity
 let parallaxIntensityXFactor: CGFloat = 0.5
+
+// A factor applied in the y axis on parallax intensity
 let parallaxIntensityYFactor: CGFloat = 0.7
 
-let rotateYFactor: CGFloat = 16
-
+// A factor applied in the x axis on rotation
 let rotateXFactor: CGFloat = 20
 
+// A factor applied in the y axis on rotation
+let rotateYFactor: CGFloat = 16
+
+// A factor applied in the z axis on rotation
 let rotateZFactor: CGFloat = 9
 
+// Determines the size of the TVButton's shadow in comparison to the bounds
 let shadowFactor: CGFloat = 12
 
+// The scale of the specular shine
 let specularScale: CGFloat = 1.7
